4309fe6d8e494a53cc0969c9d3bc2e7143ad072a
unknown <kayanabdepbaki@gmail.com>
2026-04-24 04:12:42 +0300

    Allow local frontend port 3000 in CORS

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ load_dotenv()
 default_origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 frontend_origins = [
